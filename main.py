43ce161bd4a870f6d831a2f6744579308a65c555
ArthurAllioux <arthur.allioux@epfedu.fr>
2021-06-11 12:44:59 +0200

    Main finished

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
 import streamlit as st
-st.title("Hello world")
+import logging
+import requests
+import pytrends
+import pandas as pd
+import lxml
+from pytrends.request import TrendReq
+import matplotlib.pyplot as plt
+import os
+import re
+import functools
+import time
+import collections
+
+st.title("Hello world ! It's Arthur ALLIOUX")
+
+if st.button('Generate joke'):
+    req = requests.get(" https://v2.jokeapi.dev/joke/Any")
+    reponse = req.json()
+    st.text(reponse['category'] + " joke received : ")
+    if reponse['type'] == 'single':
+        st.write(reponse['joke'])
+    if reponse['type'] == 'twopart':
+        st.write(reponse['setup'])
+        st.write(reponse['delivery'])
+
+
